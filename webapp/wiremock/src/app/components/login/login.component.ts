5d493b05164995748fd0098bac5b813a9c5cf541
holomekc <30546982+holomekc@users.noreply.github.com>
2025-06-20 11:39:39 +0200

    feat: update deps
    - Update node 20 to 22
    - Update angular and font awesome
    - Replace moment with date-fns
    - Cleanup

--- webapp/wiremock/src/app/components/login/login.component.ts
+++ webapp/wiremock/src/app/components/login/login.component.ts
@@ -2,13 +2,10 @@ import { Component, inject } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { LoginService } from "../../services/login.service";
 import { Router } from "@angular/router";
-import { NgIf } from "@angular/common";
-import { NgbDropdown, NgbDropdownMenu, NgbDropdownToggle } from "@ng-bootstrap/ng-bootstrap";
-import { FaIconComponent } from "@fortawesome/angular-fontawesome";
 
 @Component({
   selector: "wm-login",
-  imports: [FormsModule, NgIf, NgbDropdown, NgbDropdownToggle, FaIconComponent, NgbDropdownMenu],
+  imports: [FormsModule],
   templateUrl: "./login.component.html",
   styleUrl: "./login.component.scss",
 })
